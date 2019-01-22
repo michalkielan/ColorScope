132c0e266669416fe5616bc8f7fe460f24ae45c4
Michal Kielan <michalkielan@protonmail.com>
2019-01-22 08:09:25 +0100

    fix linter

--- colorscope.py
+++ colorscope.py
@@ -102,7 +102,7 @@ class ColorReader(metaclass=abc.ABCMeta):
       self.__rect[1] = [x, y]
       if self.__rect[0] != self.__rect[1]:
         color = self.read_rect_color(self.__rect)
-        print(color[0], color[1], color[2], sep = '\t')
+        print(color[0], color[1], color[2], sep='\t')
 
   def processing(self):
     cv2.imshow(self.__window, self._img)
